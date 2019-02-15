ee029229e45c89552f22a06a8d2607322a5cded2
EssKayz <esskay@protonmail.com>
2019-02-15 23:44:43 +0200

    Update StringMethods.java
    Comments for methods

--- src/main/java/TiraLab/Structures/StringMethods.java
+++ src/main/java/TiraLab/Structures/StringMethods.java
@@ -11,6 +11,12 @@ package TiraLab.Structures;
  */
 public class StringMethods {
 
+        /**
+     * Get the index of the last occurrence of a substring from another string
+     * @param toFind the substring to be found
+     * @param fullstring the full string to be searched from
+     * @return the index of the first letter of the last occurrence of the substring
+     */
     public int lastIndexOf(String toFind, String fullstring) {
         int lastIndex = -1;
         for (int i = fullstring.length() - toFind.length(); i > 0; i--) {
@@ -31,6 +37,14 @@ public class StringMethods {
         return lastIndex;
     }
 
+    /**
+     * Get the index of the first occurence of a substring from another string, starting from the parameter index
+     *
+     * @param toFind the substring to be found
+     * @param fullstring the full string to be searched from
+     * @param startIndex the index to start the search from
+     * @return the index of the first character of the substring
+     */
     public int indexOf(String toFind, String fullstring, int startIndex) {
         String sub = substring(fullstring, startIndex, fullstring.length());
         int index = indexOf(toFind, sub);
@@ -40,6 +54,13 @@ public class StringMethods {
         return index + startIndex;
     }
 
+    /**
+     * Get the index of the first occurrence of a substring from another string
+     *
+     * @param toFind the substring to be found
+     * @param fullstring the full string to be searched from
+     * @return the index of the first character of the substring
+     */
     public int indexOf(String toFind, String fullstring) {
         if (toFind.length() > fullstring.length()) {
             return -1;
@@ -60,6 +81,14 @@ public class StringMethods {
         return -1;
     }
 
+    /**
+     * Get a substring of a string
+     *
+     * @param original the original string in full length
+     * @param start the index of first char of substring
+     * @param end the index of the last char of the substring
+     * @return the substring formed from start to end
+     */
     public String substring(String original, int start, int end) {
         String derp = "";
         for (int i = start; i < end; i++) {
